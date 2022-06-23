b2f142555ed47d624eadd673affc197f4e524043
Carlos Barreto <carlosmba96@gmail.com>
2022-06-23 15:39:25 -0400

    update

--- src/js/script.js
+++ src/js/script.js
@@ -6,47 +6,50 @@ const expRegCity = /^[A-Z]+$/i;
 const errorMsg = document.querySelector("span");
 const apiKey = "506bbcbbb0bf0d7bbf2ad10113bd22f4";
 
-inputCity.addEventListener("click", () => {
-  errorMsg.textContent = "";
-});
+const weatherModule = (() => {
+  const getWeatherData = async ({ lat, lon }) => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+      );
 
-const getWeatherData = async ({ lat, lon }) => {
-  try {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
-    );
+      const weatherData = await response.json();
+      const weatherActual = weatherData["weather"];
+      console.log(weatherData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    const weatherData = await response.json();
-    console.log(weatherData);
-  } catch (error) {
-    console.log(error);
-  }
-};
+  const getGeoLocate = async (city) => {
+    try {
+      const response = await fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`
+      );
 
-const getGeoLocate = async (city) => {
-  try {
-    const response = await fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`
-    );
+      const dataCity = await response.json();
 
-    const dataCity = await response.json();
-    console.log(dataCity);
+      const geoLocateCity = { lat: dataCity[0].lat, lon: dataCity[0].lon };
+      getWeatherData(geoLocateCity);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    const geoLocateCity = { lat: dataCity[0].lat, lon: dataCity[0].lon };
-
-    getWeatherData(geoLocateCity);
-  } catch (error) {
-    console.log(error);
-  }
-};
+  return { getGeoLocate };
+})();
 
 const checkInput = () => {
   if (expRegCity.test(inputCity.value)) {
-    getGeoLocate(inputCity.value);
+    weatherModule.getGeoLocate(inputCity.value);
   } else {
     errorMsg.textContent = "this field should only contain letters";
   }
 };
+
+inputCity.addEventListener("click", () => {
+  errorMsg.textContent = "";
+});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
